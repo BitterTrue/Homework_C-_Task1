50dc59ec65c1d8d26fea9bdf55a5c86fec31f348
Yana <vip.yana.baranova.2000@mail.ru>
2023-06-10 16:16:36 +0300

    first task

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first number:");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int secondNumber = int.Parse(Console.ReadLine());
+int max = firstNumber > secondNumber? firstNumber : secondNumber;
+int min = firstNumber < secondNumber? firstNumber : secondNumber;
+Console.WriteLine($"max={max}, min={min}");
