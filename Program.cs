2be644a494cfd57aae2f139247c7a685b48e71e8
BitterTrue <134939407+BitterTrue@users.noreply.github.com>
2023-06-10 16:38:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
 Console.WriteLine("Enter first number:");
 int firstNumber = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter second number:");
 int secondNumber = int.Parse(Console.ReadLine());
+
 int max = firstNumber > secondNumber? firstNumber : secondNumber;
 int min = firstNumber < secondNumber? firstNumber : secondNumber;
+
 Console.WriteLine($"max={max}, min={min}");
